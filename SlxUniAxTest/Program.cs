a527461b162a6d0cc6d898ce4a992d252337992d
ACA@GIANOS <alberto.chiesa@gianos.it>
2011-11-08 09:17:52 -0700

    Test su Console

--- SlxUniAxTest/Program.cs
+++ SlxUniAxTest/Program.cs
@@ -11,8 +11,19 @@ namespace Gianos.SlxUniAxTest
         static void Main(string[] args)
         {
             //var dbHandler = new DbHandler(".\\ACA2008", "BVSLX_PROD", "sa", "masterkey");
-            var dbHandler = new DbHandler(".\\ACA2008", "BVSLX_PROD", "sysdba", "masterkey");
+            //var dbHandler = new DbHandler(".\\ACA2008", "BVSLX_PROD", "sysdba", "masterkey");
+            //var fields = dbHandler.ReadTableDataFromSLXDb();
 
+            var model = new SLXModelHandler(@"C:\Users\ACA.GIANOS\Documents\Dev\bvweb\Model");
+            var fields = model.FindEntityModels();
+            var dbHandler = new DbHandler(".\\ACA2008", "BVSLX_PROD", "sysdba", "masterkey");
+            fields = dbHandler.ReadTableDataFromSLXDb(fields);
+            var f = fields["BVSTORE"]["COD_COUNTRY"];
+
+            //model.SetUnicodeOnSlxField(f, true);
+            dbHandler.SetUnicodeOnDbField(f, true);
+            //var s = dbHandler.GetCreateScriptForIndexes("C_BASE_TABLE", "NETSALEEUR");
+            //Console.Write(s);
         }
     }
 }
